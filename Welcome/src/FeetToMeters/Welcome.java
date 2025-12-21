825262e2b485e2ccdf74499cc0bf0b8a2f4a436f
Hadeeqa Yaseen <Hadeeqayaseen@gmail.com>
2025-12-22 02:42:59 +0800

    Initial commit

---
+++ Welcome/src/FeetToMeters/Welcome.java
@@ -0,0 +1,22 @@
+package FeetToMeters;
+
+import java.util.Scanner;
+
+//public class FeetToMeters {
+//    public static void main(String[] args) {
+//        // Create a Scanner object for input
+//        Scanner input = new Scanner(System.in);
+//
+//        // Prompt the user to enter a value in feet
+//        System.out.print("Enter a value for feet: ");
+//        double feet = input.nextDouble();
+//
+//        // Convert feet to meters
+//        double meters = feet * 0.305;
+//
+//        // Display the result
+//        System.out.println(feet + " feet is " + meters + " meters.");
+//        
+//        input.close();
+//    }
+//}
